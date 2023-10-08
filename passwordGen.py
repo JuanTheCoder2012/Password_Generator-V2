c8d4f2c1b73aac4d64be673ba1898c444ff2db5a
JuanTheCoder2012 <107782146+JuanTheCoder2012@users.noreply.github.com>
2023-10-07 20:18:25 -0400

    i see now it is animating but.. it is not outputting anything

--- passwordGen.py
+++ passwordGen.py
@@ -1,16 +1,24 @@
 import tkinter as tk
-from tkinter import messagebox
 import random
 import string
+from tkinter import messagebox
 
-# Function to generate a random password
-def generate_password():
+# Function to generate a random password with animation
+def generate_password_with_animation():
     length = int(length_entry.get())
     if length < 8:
         messagebox.showerror("Error", "Password length must be at least 8 characters")
     else:
         characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(characters) for _ in range(length))
+        generated_password = ''.join(random.choice(characters) for _ in range(length))
+        animate_password(generated_password)
+
+# Function to animate the generated password
+def animate_password(password):
+    if password:
+        result_label.config(text="Generated Password: " + password[0])
+        root.after(100, lambda: animate_password(password[1:]))
+    else:
         result_label.config(text="Generated Password: " + password)
 
 # Create the main window
@@ -23,8 +31,8 @@ length_label.pack()
 length_entry = tk.Entry(root)
 length_entry.pack()
 
-# Button to generate password
-generate_button = tk.Button(root, text="Generate Password", command=generate_password)
+# Button to generate password with animation
+generate_button = tk.Button(root, text="Generate Password with Animation", command=generate_password_with_animation)
 generate_button.pack()
 
 # Label to display generated password
