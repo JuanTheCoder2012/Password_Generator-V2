33d9c08675d8dea263def239d7b100e072636ed1
JuanTheCoder2012 <107782146+JuanTheCoder2012@users.noreply.github.com>
2023-10-07 20:07:56 -0400

    i managed to reset it all I started all over now I want to add animation.

--- passwordGen.py
+++ passwordGen.py
@@ -3,39 +3,33 @@ from tkinter import messagebox
 import random
 import string
 
-# Function to generate a random password and animate it
-def generate_and_animate_password():
+# Function to generate a random password
+def generate_password():
     length = int(length_entry.get())
     if length < 8:
-        messagebox.showerror("Error", "Password length should be at least 8 characters.")
+        messagebox.showerror("Error", "Password length must be at least 8 characters")
     else:
-        all_characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(all_characters) for _ in range(length))
-        animate_password(password, 0)
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for _ in range(length))
+        result_label.config(text="Generated Password: " + password)
 
-# Function to animate the password letter by letter
-def animate_password(password, index):
-    if index < len(password):
-        password_entry.delete(0, tk.END)
-        password_entry.insert(tk.END, password[:index+1])
-        index += 1
-        password_entry.after(100, animate_password, password, index)
-
-# GUI setup
+# Create the main window
 root = tk.Tk()
 root.title("Password Generator")
 
-length_label = tk.Label(root, text="Enter password length:")
+# Label and entry for password length
+length_label = tk.Label(root, text="Enter Password Length:")
 length_label.pack()
-
-length_entry = tk.Entry(root, width=20)
+length_entry = tk.Entry(root)
 length_entry.pack()
 
-generate_button = tk.Button(root, text="Generate Password", command=generate_and_animate_password)
+# Button to generate password
+generate_button = tk.Button(root, text="Generate Password", command=generate_password)
 generate_button.pack()
 
-password_entry = tk.Entry(root, width=30, show="*")
-password_entry.pack()
+# Label to display generated password
+result_label = tk.Label(root, text="")
+result_label.pack()
 
+# Run the GUI main loop
 root.mainloop()
-
